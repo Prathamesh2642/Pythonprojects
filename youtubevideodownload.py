a6edd2ff331b1d652dd0f725e42fd81fd3751a8e
Prathamesh2642 <76043914+Prathamesh2642@users.noreply.github.com>
2022-12-18 11:18:15 +0530

    Create youtubevideodownload.py

---
+++ youtubevideodownload.py
@@ -0,0 +1,9 @@
+from pytube import YouTube
+url=input("Enter the link of the video ")
+myvideo=YouTube(url)
+print(myvideo.title)
+# print(myvideo.thumbnail_url)
+# for streams in myvideo.streams:
+    # print(streams)
+downloadvid=myvideo.streams.get_highest_resolution()
+downloadvid.download()
